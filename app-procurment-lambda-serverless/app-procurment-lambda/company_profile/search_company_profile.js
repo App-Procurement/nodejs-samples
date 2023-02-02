129b570f778b3491076a5a2b0b0edaa314e4bbf2
JitinKGoyal <jkgoyal85@gmail.com>
2023-02-02 12:43:10 +0530

    procurement lambda excel documentation

---
+++ app-procurment-lambda-serverless/app-procurment-lambda/company_profile/search_company_profile.js
@@ -0,0 +1,77 @@
+exports.search_company_profile = async (event, context, callback) => {
+
+    const { Client } = require('pg');
+
+    const client = new Client({
+        host: "postgresql.ch8wfucynpvq.us-east-1.rds.amazonaws.com",
+        port: "5431",
+        database: "procurement",
+        user:"postgres",
+        password: "P0$tGr3$&s3qua1$n3t!k5"
+    });
+    client.connect();
+    
+    let data = {};
+    
+    if ( event.queryStringParameters) {
+        data =  event.queryStringParameters;
+    }
+    
+    const filters = data;
+    
+    let keysArr = Object.keys(filters);
+    let valueArr = Object.values(filters);
+    let abc;
+    let objReturn = {
+        code: 200,
+        message: "company_profile search successfully",
+        type: "object",
+        object: []
+    };
+
+    try {
+        if (JSON.stringify(data) === '{}') {
+
+            abc = await client.query(`SELECT * FROM company_profile`);
+        } else if (data.id) {
+
+            abc = await client.query(`SELECT * FROM company_profile WHERE id=$1`, [data.id]);
+        }
+        else {
+
+            for (let item of keysArr) {
+
+                abc = await client.query(`SELECT * FROM company_profile WHERE details->$1 @> $2`, [item, JSON.stringify(valueArr[keysArr.indexOf(item)])]);
+            }
+
+        }
+        objReturn.object = abc.rows;
+        client.end();
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin":"*"
+            },
+            "body": JSON.stringify(objReturn) 
+        };
+        
+    } catch (e) {
+
+        objReturn.code = 400;
+        objReturn.message = e;
+        client.end();
+        
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Access-Control-Allow-Origin":"*"
+            },
+            "body": JSON.stringify(objReturn) 
+        };
+
+    }
+
+};
+
+
---
+++ app-procurment-lambda-serverless/app-procurment-lambda/invoice/delete_invoice.js
@@ -0,0 +1,85 @@
+exports.delete_invoice = async (event, context, callback) => {
+    event = JSON.parse(event.body)
+
+
+    const { Client } = require('pg');
+
+    const client = new Client({
+        host: "postgresql.ch8wfucynpvq.us-east-1.rds.amazonaws.com",
+        port: "5431",
+        database: "procurement",
+        user: "postgres",
+        password: "P0$tGr3$&s3qua1$n3t!k5"
+    });
+
+    client.connect();
+
+    let objReturn = {
+        code: 200,
+        message: "invoice delete successfully",
+        type: "object",
+        object: []
+    };
+
+    try {
+        if (event.id) {
+
+            const res = await client.query(`DELETE FROM invoice WHERE id = $1`, [event.id]);
+
+            if (res.rowCount == 1) {
+
+                return {
+                    "statusCode": 200,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body": JSON.stringify(objReturn)
+                };
+
+            } else {
+
+                objReturn.code = 404;
+                objReturn.message = "id does not exist";
+
+                client.end();
+                return {
+                    "statusCode": 404,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body": JSON.stringify(objReturn)
+                };
+            }
+
+        } else {
+
+            objReturn.code = 404;
+            objReturn.message = "input json must have an 'id' field";
+
+            client.end();
+            return {
+                "statusCode": 404,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                "body": JSON.stringify(objReturn)
+            };
+        }
+
+
+    } catch (e) {
+        objReturn.code = 400;
+        objReturn.message = e;
+        client.end();
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": JSON.stringify(objReturn)
+        };
+    }
+
+};
+
+
